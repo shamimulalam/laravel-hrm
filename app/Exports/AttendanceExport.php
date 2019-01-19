1694c4525caffde36d63f74157f42a02c0764b3a
Shamimul Alam <bd.shawon.edu@gmail.com>
2019-01-19 18:59:14 +0600

    Export with custom fields and heading

--- app/Exports/AttendanceExport.php
+++ app/Exports/AttendanceExport.php
@@ -6,8 +6,9 @@ use App\Attendance;
 use Maatwebsite\Excel\Concerns\Exportable;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class AttendanceExport implements FromQuery
+class AttendanceExport implements FromQuery, WithHeadings
 {
     protected $user_id;
     public function __construct($user_id)
@@ -21,10 +22,20 @@ class AttendanceExport implements FromQuery
     */
     public function query()
     {
-        return Attendance::query()->where('user_id',$this->user_id);
+        return Attendance::query()->select('id','date','in_time','out_time','status')->where('user_id',$this->user_id);
     }
     /*public function collection()
     {
         return Attendance::all();
     }*/
+    public function headings(): array
+    {
+        return [
+            'Serial',
+            'Date',
+            'In Time',
+            'Out Time',
+            'Status'
+        ];
+    }
 }
